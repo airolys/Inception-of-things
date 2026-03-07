93c237c88c6559b7584c8f76ba80fb46dc91f8ef
Romain Kassel <rkassel@Ordinateur-portable-de-Romain.local>
2026-03-07 16:53:54 +0100

    adding script to setup docker and k3d on VM

---
+++ p3/scripts/setup.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+# ************************************************************************** */
+# Setup Docker Engine                                                        */
+# ************************************************************************** */
+
+# Source: official Docker documentation: https://docs.docker.com/engine/install/debian/
+
+# Set up Docker's apt repository.
+# Add Docker's official GPG key:
+sudo apt update
+sudo apt install ca-certificates curl -y
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+sudo tee /etc/apt/sources.list.d/docker.sources <<EOF
+Types: deb
+URIs: https://download.docker.com/linux/debian
+Suites: $(. /etc/os-release && echo "$VERSION_CODENAME")
+Components: stable
+Signed-By: /etc/apt/keyrings/docker.asc
+EOF
+
+sudo apt update
+
+# Install Docker packages
+sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+
+# Verify that Docker is running
+sudo systemctl status docker
+
+# Verify that the installation is successful by running the hello-world image
+sudo docker run hello-world
+
+# ************************************************************************** */
+# Setup k3d                                                                  */
+# ************************************************************************** */
+
+# Install current latest release of k3d
+# Source: official k3d documentation: https://k3d.io/stable/#installation
+curl -s https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
+
+# Create a cluster named mycluster with just a single server node for testing purposes
+# Source: official k3d documentation: https://k3d.io/stable/#quick-start
+sudo k3d cluster create mycluster
